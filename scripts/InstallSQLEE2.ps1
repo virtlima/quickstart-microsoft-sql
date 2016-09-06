9689ab287105558a2a75034f5cf16deef48710d4
Jay McConnell <jmmccon@amazon.com>
2016-09-06 16:34:14 +0200

    various fixes for installSQLEE2.ps1

--- scripts/InstallSQLEE2.ps1
+++ scripts/InstallSQLEE2.ps1
@@ -34,16 +34,28 @@ try {
     $DomainAdminFullUser = $DomainNetBIOSName + '\' + $DomainAdminUser
     $DomainAdminSecurePassword = ConvertTo-SecureString $DomainAdminPassword -AsPlainText -Force
     $DomainAdminCreds = New-Object System.Management.Automation.PSCredential($DomainAdminFullUser, $DomainAdminSecurePassword)
+    Enable-WSManCredSSP Client –DelegateComputer * -Force
+    Enable-WSManCredSSP Server -Force
+
     $InstallSqlPs={
-        $ErrorActionPreference = "Stop"
         Install-WindowsFeature NET-Framework-Core
-        dir \\$args[0]\sqlinstall\*.iso | Mount-DiskImage
-        g:\SETUP.EXE /QS /Action=Install /Features=SQLEngine,Replication,FullText,Conn,BOL,ADV_SSMS /INSTANCENAME=MSSQLSERVER /SQLSVCACCOUNT=$args[1]\$args[2] /SQLSVCPASSWORD=$args[3] /AGTSVCACCOUNT=$args[4]\$args[5] /AGTSVCPASSWORD=$args[6] /SQLSYSADMINACCOUNTS=$args[7]\$args[8] /SQLUSERDBDIR='D:\MSSQL\DATA' /SQLUSERDBLOGDIR='E:\MSSQL\LOG' /SQLBACKUPDIR='f:\MSSQL\Backup' /SQLTEMPDBDIR='f:\MSSQL\TempDB' /SQLTEMPDBLOGDIR='f:\MSSQL\TempDB' /IACCEPTSQLSERVERLICENSETERMS
+        $ErrorActionPreference = "Stop"
+        Enable-WSManCredSSP Server -Force
+        $share= "//" + $args[0] + "/sqlinstall/"
+        $fname= $share + (dir -File -Path $share)
+        echo $fname
+        Mount-DiskImage -ImagePath $fname
+        $installer = "G:\SETUP.EXE"
+        $arguments =  '/Q /Action=Install /UpdateEnabled=False /Features=SQLEngine,Replication,FullText,Conn,BOL,ADV_SSMS /INSTANCENAME=MSSQLSERVER /SQLSVCACCOUNT="' + $args[1] + '\' + $args[2] + '" /SQLSVCPASSWORD="' + $args[3] + '" /AGTSVCACCOUNT="' + $args[4] + '\' + $args[5] + '" /AGTSVCPASSWORD="' + $args[6] + '" /SQLSYSADMINACCOUNTS="' + $args[7] + '\' + $args[8] + '" /SQLUSERDBDIR="D:\MSSQL\DATA" /SQLUSERDBLOGDIR="E:\MSSQL\LOG" /SQLBACKUPDIR="F:\MSSQL\Backup" /SQLTEMPDBDIR="F:\MSSQL\TempDB" /SQLTEMPDBLOGDIR="F:\MSSQL\TempDB" /IACCEPTSQLSERVERLICENSETERMS'
+        echo $installer $arguments
+        Start-Process $installer $arguments -Wait -RedirectStandardOutput "C:\cfn\log\SQLInstallerOutput.txt" -RedirectStandardError "C:\cfn\log\SQLInstallerErrors.txt"    
         C:\PROGRA~1\MICROS~1\CLIENT~1\ODBC\110\Tools\Binn\SQLCMD.EXE -i c:\cfn\scripts\MaxDOP.sql
     }
-    Invoke-Command -Scriptblock $InstallSqlPs -ComputerName localhost -Credential $DomainAdminCreds -ArgumentList $ADServerNetBIOSName,$DomainNetBIOSName,$SQLServiceAccount,$SQLServiceAccountPassword,$DomainNetBIOSName,$SQLServiceAccount,$SQLServiceAccountPassword,$DomainNetBIOSName,$DomainAdminUser
+
+    Invoke-Command -Authentication Credssp -Scriptblock $InstallSqlPs -ComputerName WSFCNode11 -Credential $DomainAdminCreds -ArgumentList $ADServerNetBIOSName,$DomainNetBIOSName,$SQLServiceAccount,$SQLServiceAccountPassword,$DomainNetBIOSName,$SQLServiceAccount,$SQLServiceAccountPassword,$DomainNetBIOSName,$DomainAdminUser
 
 }
 catch {
+    echo $_
     $_ | Write-AWSQuickStartException
 }

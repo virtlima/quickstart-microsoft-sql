93b73cb024e83005ebb664e201c1ec2fffd5ef5e
Jay McConnell <jmmccon@amazon.com>
2016-09-07 09:53:35 +0200

    split install scripts into components

--- scripts/InstallSQLEE2.ps1
+++ scripts/InstallSQLEE2.ps1
@@ -6,6 +6,10 @@ param(
     $ADServerNetBIOSName,
 
     [Parameter(Mandatory=$true)]
+    [string]
+    $NetBIOSName,
+
+    [Parameter(Mandatory=$true)]
     [string]    
     $DomainNetBIOSName,
 
@@ -34,25 +38,18 @@ try {
     $DomainAdminFullUser = $DomainNetBIOSName + '\' + $DomainAdminUser
     $DomainAdminSecurePassword = ConvertTo-SecureString $DomainAdminPassword -AsPlainText -Force
     $DomainAdminCreds = New-Object System.Management.Automation.PSCredential($DomainAdminFullUser, $DomainAdminSecurePassword)
-    Enable-WSManCredSSP Client -DelegateComputer * -Force
-    Enable-WSManCredSSP Server -Force
 
     $InstallSqlPs={
-        Install-WindowsFeature NET-Framework-Core
         $ErrorActionPreference = "Stop"
-        Enable-WSManCredSSP Server -Force
         $share= "//" + $args[0] + "/sqlinstall/"
         $fname= $share + (dir -File -Path $share)
-        echo $fname
         Mount-DiskImage -ImagePath $fname
         $installer = "G:\SETUP.EXE"
         $arguments =  '/Q /Action=Install /UpdateEnabled=False /Features=SQLEngine,Replication,FullText,Conn,BOL,ADV_SSMS /INSTANCENAME=MSSQLSERVER /SQLSVCACCOUNT="' + $args[1] + '\' + $args[2] + '" /SQLSVCPASSWORD="' + $args[3] + '" /AGTSVCACCOUNT="' + $args[4] + '\' + $args[5] + '" /AGTSVCPASSWORD="' + $args[6] + '" /SQLSYSADMINACCOUNTS="' + $args[7] + '\' + $args[8] + '" /SQLUSERDBDIR="D:\MSSQL\DATA" /SQLUSERDBLOGDIR="E:\MSSQL\LOG" /SQLBACKUPDIR="F:\MSSQL\Backup" /SQLTEMPDBDIR="F:\MSSQL\TempDB" /SQLTEMPDBLOGDIR="F:\MSSQL\TempDB" /IACCEPTSQLSERVERLICENSETERMS'
-        echo $installer $arguments
         Start-Process $installer $arguments -Wait -RedirectStandardOutput "C:\cfn\log\SQLInstallerOutput.txt" -RedirectStandardError "C:\cfn\log\SQLInstallerErrors.txt"    
-        C:\PROGRA~1\MICROS~1\CLIENT~1\ODBC\110\Tools\Binn\SQLCMD.EXE -i c:\cfn\scripts\MaxDOP.sql
     }
 
-    Invoke-Command -Authentication Credssp -Scriptblock $InstallSqlPs -ComputerName WSFCNode11 -Credential $DomainAdminCreds -ArgumentList $ADServerNetBIOSName,$DomainNetBIOSName,$SQLServiceAccount,$SQLServiceAccountPassword,$DomainNetBIOSName,$SQLServiceAccount,$SQLServiceAccountPassword,$DomainNetBIOSName,$DomainAdminUser
+    Invoke-Command -Authentication Credssp -Scriptblock $InstallSqlPs -ComputerName $NetBIOSName -Credential $DomainAdminCreds -ArgumentList $ADServerNetBIOSName,$DomainNetBIOSName,$SQLServiceAccount,$SQLServiceAccountPassword,$DomainNetBIOSName,$SQLServiceAccount,$SQLServiceAccountPassword,$DomainNetBIOSName,$DomainAdminUser
 
 }
 catch {
---
+++ scripts/SetMaxDOP.ps1
@@ -0,0 +1,42 @@
+[CmdletBinding()]
+param(
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $NetBIOSName,
+
+    [Parameter(Mandatory=$true)]
+    [string]    
+    $DomainNetBIOSName,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $DomainAdminUser,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $DomainAdminPassword
+
+)
+
+try {
+    Start-Transcript -Path C:\cfn\log\SetMaxDOP.ps1.txt -Append
+    $ErrorActionPreference = "Stop"
+
+    $DomainAdminFullUser = $DomainNetBIOSName + '\' + $DomainAdminUser
+    $DomainAdminSecurePassword = ConvertTo-SecureString $DomainAdminPassword -AsPlainText -Force
+    $DomainAdminCreds = New-Object System.Management.Automation.PSCredential($DomainAdminFullUser, $DomainAdminSecurePassword)
+
+    $SetupMaxDOPPs={
+        $ErrorActionPreference = "Stop"
+        $maxdop = "C:\PROGRA~1\MICROS~1\CLIENT~1\ODBC\110\Tools\Binn\SQLCMD.EXE"
+        $arguments = "-i c:\cfn\scripts\MaxDOP.sql"
+        Start-Process $maxdop $arguments -Wait -RedirectStandardOutput "C:\cfn\log\MaxDOPOutput.txt" -RedirectStandardError "C:\cfn\log\MaxDOPErrors.txt"    
+    }
+
+    Invoke-Command -Authentication Credssp -Scriptblock $SetupMaxDOPPs -ComputerName $NetBIOSName -Credential $DomainAdminCreds
+
+}
+catch {
+    $_ | Write-AWSQuickStartException
+}

3deab4f2ee69989fe905d72610c0739bc07863e8
Jay McConnell <jmmccon@amazon.com>
2016-09-08 09:42:27 +0200

    cleanup indents and add argumentlist to Invoke-Command

--- scripts/Enable-AlwaysOn.ps1
+++ scripts/Enable-AlwaysOn.ps1
@@ -3,15 +3,12 @@ param(
 
     [Parameter(Mandatory=$true)]
     [string]$DomainNetBIOSName,
-    
+
     [Parameter(Mandatory=$true)]
     [string]$DomainAdminUser,
-    
+
     [Parameter(Mandatory=$true)]
     [string]$DomainAdminPassword,
-
-    [Parameter(Mandatory=$false)]
-    [string]$ServerName='localhost',
 
     [Parameter(Mandatory=$true)]
     [string]$WSFCNode1NetBIOSName,
@@ -28,18 +25,18 @@ try {
     $DomainAdminSecurePassword = ConvertTo-SecureString $DomainAdminPassword -AsPlainText -Force
     $DomainAdminCreds = New-Object System.Management.Automation.PSCredential($DomainAdminFullUser, $DomainAdminSecurePassword)
 
-$Enable-AlwaysOn-WFCNode1={
+    $Enable-AlwaysOn-WFCNode1={
         $ErrorActionPreference = "Stop"
-        Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned;Enable-SqlAlwaysOn -ServerInstance $WSFCNode1NetBIOSName -Force 
+        Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned;Enable-SqlAlwaysOn -ServerInstance $args[0] -Force
     }
 
-$Enable-AlwaysOn-WFCNode2={
+    $Enable-AlwaysOn-WFCNode2={
         $ErrorActionPreference = "Stop"
-        Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned;Enable-SqlAlwaysOn -ServerInstance $WSFCNode2NetBIOSName -Force 
+        Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned;Enable-SqlAlwaysOn -ServerInstance $args[0] -Force
     }
 
-Invoke-Command -Scriptblock $Enable-AlwaysOn-WFCNode1 -ComputerName $WSFCNode1NetBIOSName -Credential $DomainAdminCreds
-Invoke-Command -Scriptblock $Enable-AlwaysOn-WFCNode2 -ComputerName $WSFCNode2NetBIOSName -Credential $DomainAdminCreds
+    Invoke-Command -Scriptblock $Enable-AlwaysOn-WFCNode1 -ComputerName $WSFCNode1NetBIOSName -Credential $DomainAdminCreds -ArgumentList $WSFCNode1NetBIOSName
+    Invoke-Command -Scriptblock $Enable-AlwaysOn-WFCNode2 -ComputerName $WSFCNode2NetBIOSName -Credential $DomainAdminCreds -ArgumentList $WSFCNode2NetBIOSName
 }
 catch {
     $_ | Write-AWSQuickStartException

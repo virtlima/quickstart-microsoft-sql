269b608255fa2d9813244735d347ec6b44fff56b
Jay McConnell <jmmccon@amazon.com>
2016-09-08 07:13:31 +0200

    fix syntax and params reference

--- scripts/Set-Folder-Permissions.ps1
+++ scripts/Set-Folder-Permissions.ps1
@@ -6,19 +6,18 @@ param(
 
     [Parameter(Mandatory=$true)]
     [string]$DomainAdminUser,
-    
+
     [Parameter(Mandatory=$true)]
     [string]$DomainAdminPassword,
-    
+
     [Parameter(Mandatory=$true)]
     [string]$ADServer1NetBIOSName,
-    
-    [Parameter(Mandatory=$true)]
-    [string]$SQLServiceAccount,
-    
 
+    [Parameter(Mandatory=$true)]
+    [string]$SQLServiceAccount
 
 )
+
 Try{
     Start-Transcript -Path C:\cfn\log\Set-Folder-Permissions.ps1.txt -Append
     $ErrorActionPreference = "Stop"
@@ -27,28 +26,27 @@ Try{
     $DomainAdminSecurePassword = ConvertTo-SecureString $DomainAdminPassword -AsPlainText -Force
     $DomainAdminCreds = New-Object System.Management.Automation.PSCredential($DomainAdminFullUser, $DomainAdminSecurePassword)
 
-$SetPermissions={ 
+$SetPermissions={
     $ErrorActionPreference = "Stop"
     $acl = Get-Acl C:\witness;
-    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule( $args[0]+'\WSFCluster1$','FullControl', 'ContainerInherit, ObjectInherit', 'None', 'Allow'); 
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule( $args[0] + '\WSFCluster1$','FullControl', 'ContainerInherit, ObjectInherit', 'None', 'Allow');
     $acl.AddAccessRule($rule);
     Set-Acl C:\witness $acl
 }
 
 Invoke-Command -ScriptBlock $SetPermissions -ComputerName $ADServer1NetBIOSName -Credential $DomainAdminCreds -ArgumentList $DomainNetBIOSName
 
-$SetPermissions2={ 
-$ErrorActionPreference = "Stop"
-$acl = Get-Acl C:\replica;
-$rule = New-Object System.Security.AccessControl.FileSystemAccessRule( $args[0]+'\$SQLServiceAccount','FullControl', 'ContainerInherit, ObjectInherit', 'None', 'Allow'); 
-$acl.AddAccessRule($rule);
-Set-Acl C:\replica $acl
+$SetPermissions2={
+    $ErrorActionPreference = "Stop"
+    $acl = Get-Acl C:\replica;
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule( $args[0] + '\' + $args[1], 'FullControl', 'ContainerInherit, ObjectInherit', 'None', 'Allow');
+    $acl.AddAccessRule($rule);
+    Set-Acl C:\replica $acl
 }
 
 Invoke-Command -ScriptBlock $SetPermissions -ComputerName $ADServer1NetBIOSName -Credential $DomainAdminCreds -ArgumentList $DomainNetBIOSName, $SQLServiceAccount
-                                            
+
 }
 Catch{
      $_ | Write-AWSQuickStartException
      }
-                                        

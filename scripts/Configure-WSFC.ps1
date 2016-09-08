9be2a632de9ea016916e27735fda58dc2d510c65
Jay McConnell <jmmccon@amazon.com>
2016-09-08 09:15:53 +0200

    cleanup spacing

--- scripts/Configure-WSFC.ps1
+++ scripts/Configure-WSFC.ps1
@@ -1,20 +1,18 @@
 [CmdletBinding()]
 param(
 
-    
-
     [Parameter(Mandatory=$true)]
     [string]$WSFCNode2NetBIOSName
-    
+
     [Parameter(Mandatory=$true)]
     [string]$WSFCNode1NetBIOSName
- 
+
     [Parameter(Mandatory=$true)]
     [string]$WSFCNode1PrivateIP2
-    
+
     [Parameter(Mandatory=$true)]
     [string]$WSFCNode2PrivateIP2
-       
+
 )
 try {
     Start-Transcript -Path C:\cfn\log\Configure-WSFC.ps1.txt -Append

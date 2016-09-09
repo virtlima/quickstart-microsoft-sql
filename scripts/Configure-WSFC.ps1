21835a74a2e0eb63e5a22949e5bee9224515866a
Jay McConnell <jmmccon@amazon.com>
2016-09-09 16:38:00 +0200

    New-Cluster command needs to be run as domain admin

--- scripts/Configure-WSFC.ps1
+++ scripts/Configure-WSFC.ps1
@@ -2,23 +2,52 @@
 param(
 
     [Parameter(Mandatory=$true)]
-    [string]$WSFCNode2NetBIOSName,
+    [string]$DomainNetBIOSName,
 
     [Parameter(Mandatory=$true)]
-    [string]$WSFCNode1NetBIOSName,
+    [string]$DomainAdminUser,
 
     [Parameter(Mandatory=$true)]
-    [string]$WSFCNode1PrivateIP2,
+    [string]$DomainAdminPassword,
 
     [Parameter(Mandatory=$true)]
-    [string]$WSFCNode2PrivateIP2
+    [string]$ADServer1NetBIOSName,
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $WSFCNode2NetBIOSName,
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $WSFCNode1NetBIOSName,
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $WSFCNode1PrivateIP2,
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $WSFCNode2PrivateIP2,
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $NetBIOSName
 
 )
+
 try {
     Start-Transcript -Path C:\cfn\log\Configure-WSFC.ps1.txt -Append
     $ErrorActionPreference = "Stop"
 
-    New-Cluster -Name WSFCluster1 -Node $WSFCNode1NetBIOSName, $WSFCNode2NetBIOSName -StaticAddress $WSFCNode1PrivateIP2, $WSFCNode2PrivateIP2
+    $DomainAdminFullUser = $DomainNetBIOSName + '\' + $DomainAdminUser
+    $DomainAdminSecurePassword = ConvertTo-SecureString $DomainAdminPassword -AsPlainText -Force
+    $DomainAdminCreds = New-Object System.Management.Automation.PSCredential($DomainAdminFullUser, $DomainAdminSecurePassword)
+
+    $ConfigWSFCPs={
+        New-Cluster -Name WSFCluster1 -Node $args[0], $args[1] -StaticAddress $args[2], $args[3]
+    }
+
+    Invoke-Command -Authentication Credssp -Scriptblock $ConfigWSFCPs -ComputerName $NetBIOSName -Credential $DomainAdminCreds -ArgumentList $WSFCNode1NetBIOSName,$WSFCNode2NetBIOSName,$WSFCNode1PrivateIP2,$WSFCNode2PrivateIP2
 }
 catch {
     $_ | Write-AWSQuickStartException

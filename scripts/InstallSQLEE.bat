9ba8328d0e365b6258883b1ab0dd423d66c0afbf
Jay McConnell <jmmccon@amazon.com>
2016-09-14 10:24:16 +0200

    removed unneeded batch scripts

--- scripts/InstallSQLEE.bat
+++
@@ -1,7 +0,0 @@
-powershell.exe -command Install-WindowsFeature NET-Framework-Core
-
-powershell.exe -command "dir \\$ADServerNetBIOSName\sqlinstall\*.iso | Mount-DiskImage"
-
-g:\SETUP.EXE /QS /Action=Install /Features=SQLEngine,Replication,FullText,Conn,BOL,ADV_SSMS /INSTANCENAME=MSSQLSERVER /SQLSVCACCOUNT=$DomainNetBIOSName\$SQLServiceAccount /SQLSVCPASSWORD=$SQLServiceAccountPassword /AGTSVCACCOUNT=$DomainNetBIOSName\$SQLServiceAccount /AGTSVCPASSWORD=$SQLServiceAccountPassword /SQLSYSADMINACCOUNTS=$DomainNetBIOSName\$DomainAdminUser /SQLUSERDBDIR="D:\MSSQL\DATA" /SQLUSERDBLOGDIR="E:\MSSQL\LOG" /SQLBACKUPDIR="f:\MSSQL\Backup" /SQLTEMPDBDIR="f:\MSSQL\TempDB" /SQLTEMPDBLOGDIR="f:\MSSQL\TempDB" /IACCEPTSQLSERVERLICENSETERMS
-
-C:\PROGRA~1\MICROS~1\CLIENT~1\ODBC\110\Tools\Binn\SQLCMD.EXE -i c:\cfn\scripts\MaxDOP.sql

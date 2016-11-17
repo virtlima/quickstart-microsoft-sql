ac0b6ba40a3d26fb74f0f69dc07ae539356d344e
Jay McConnell <jmmccon@amazon.com>
2016-11-17 07:33:30 +0200

    add retry logic to feature install

--- scripts/Install-NETFrameworkCore.ps1
+++ scripts/Install-NETFrameworkCore.ps1
@@ -7,7 +7,24 @@ try {
     Start-Transcript -Path C:\cfn\log\Install-NetFrameworkCore.ps1.txt -Append
     $ErrorActionPreference = "Stop"
 
-    Install-WindowsFeature NET-Framework-Core
+    $Retries = 0
+    $Installed = $false
+	while (($Retries -lt 4) -and (!$Installed)) {
+        try {
+            Install-WindowsFeature NET-Framework-Core
+            $Installed = $true
+        }
+        catch {
+            $Exception = $_
+            $Retries++
+            if ($Retries -lt 4) {
+                Start-Sleep ($Retries * 60)
+            }
+        }
+    }
+    if (!$Installed) {
+          throw $Exception
+    }
 }
 catch {
     $_ | Write-AWSQuickStartException

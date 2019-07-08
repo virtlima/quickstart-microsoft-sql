ba48cc383706f247f370b9634651e47953de6396
Khalid J Hosein <kjhosein@gmail.com>
2019-07-08 08:54:44 -0400

    Add -Force param to directory creation (#46)
    Make idempotent; prevent errors on additional runs.

--- scripts/install-sql-modules.ps1
+++ scripts/install-sql-modules.ps1
@@ -18,7 +18,7 @@ Install-Module SqlServer -Force -AllowClobber
 Get-NetFirewallProfile | Set-NetFirewallProfile -Enabled False
 
 "Creating Directory for DSC Public Cert"
-New-Item -Path C:\AWSQuickstart\publickeys -ItemType directory 
+New-Item -Path C:\AWSQuickstart\publickeys -ItemType directory -Force
 
 "Setting up DSC Certificate to Encrypt Credentials in MOF File"
 $cert = New-SelfSignedCertificate -Type DocumentEncryptionCertLegacyCsp -DnsName 'AWSQSDscEncryptCert' -HashAlgorithm SHA256
